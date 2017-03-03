744b5b20ffe712df9c616ba0c59fddd790046373
Michaja van der Zouwen <ITMicaH@users.noreply.github.com>
2017-03-03 15:15:18 +0100

    Update KMSActivation.ps1

--- Windows/KMSActivation.ps1
+++ Windows/KMSActivation.ps1
@@ -9,6 +9,10 @@
 .PARAMETER Domain
     This parameter is used to determine in which domain the script will search for the KMS host.
     Use the FQDN format. When not specified the script will assume the local domain.
+.PARAMETER ClientKey
+    KMS Client key to use for activation.
+.PARAMETER AutoDetectKey
+    Autodetect KMS Client key to use for activation. Needs internet connection.
 .PARAMETER Force
     With this parameter you will not be prompted to continue if windows is already activated.
 .NOTES
@@ -17,9 +21,19 @@
     
     The Get-ActivationStatus function was written by Bryan Lipscy and is available at
     http://social.technet.microsoft.com/wiki/contents/articles/5675.determine-windows-activation-status-with-powershell.aspx
+
+    ChangeLog:
+    ===========
+    Version : 1.1
+    Date    : 3-3-2017
+    Changes : Better parameter support and replaced the list of client keys with a function to retreive a list from internet.
+
 .EXAMPLE
-    .\Activate-KMSClient -Domain contoso.com
-    This will search the contoso.com domain for a KMS host and, when found, activate your windows OS.
+    .\Activate-KMSClient -Domain contoso.com -ClientKey NPPR9-FWDCX-D2C8J-H872K-2YT43
+    This will search the contoso.com domain for a KMS host and, when found, activate your windows OS suing the provided client key.
+.EXAMPLE
+    .\Activate-KMSClient -AutoDetectKey
+    This will search the local computer's domain for a KMS host and, when found, activate your windows OS using the client key retreived from internet.
 .LINK
 http://itmicah.wordpress.com/
 
@@ -27,11 +41,24 @@ http://itmicah.wordpress.com/
 function Activate-KMSClient
 {
 
-    [CmdletBinding(SupportsShouldProcess=$true)]
+    [CmdletBinding(DefaultParameterSetName='Key',SupportsShouldProcess=$true)]
     param (
         # Domain to search (FQDN)
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='Key')]
+        [Parameter(Mandatory=$false,
+                   ParameterSetName='AutoDetect')]
         [string]
-        $Domain = $env:USERDNSDOMAIN,
+        $Domain,
+
+        [Parameter(Mandatory=$true,
+                   ParameterSetName='Key')]
+        [string]
+        $ClientKey,
+
+        [Parameter(ParameterSetName='AutoDetect')]
+        [switch]
+        $AutoDetectKey,
 
         [switch]
         $Force
@@ -46,13 +73,15 @@ function Activate-KMSClient
 
     #region Functions
 
-    function IsAdministrator {
+    function IsAdministrator
+    {
         $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
         $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
         $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
     }
 
-    function Get-ActivationStatus {
+    function Get-ActivationStatus
+    {
         [CmdletBinding()]
         param(
             [Parameter(ValueFromPipeline = $true,
@@ -92,10 +121,58 @@ function Activate-KMSClient
         }
     }
 
+    function Get-KMSClientKeys
+    {
+        Param ($Url = 'https://technet.microsoft.com/en-us/library/jj612867(v=ws.11).aspx')
+        try
+        {
+            $Site = Invoke-WebRequest -Uri $Url -ErrorAction Stop
+        }
+        catch
+        {
+            throw "Unable to reach urkl '$Url'."
+        }
+        $Tables = @($Site.ParsedHtml.getElementsByTagName('TABLE'))
+        foreach ($Table in $Tables)
+        {
+            Try
+            {
+                [xml]$XMLTable = $Table.innerHTML.ToString().Replace('scope=col','')
+                $Cells = $XMLTable.GetElementsByTagName('TD')
+                If ($Cells[0].'data-th' -eq 'Operating system edition')
+                {
+                    for ($i = 0; $i -lt $Cells.count; $i++)
+                    { 
+                        switch -w ($Cells[$i].'data-th')
+                        {
+                            Operating* {$OS = $Cells[$i].P}
+                            KMS*       {
+                                [pscustomobject]@{
+                                    OperatingSystem = $OS
+                                    KMSClientKey = $Cells[$i].P
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {}
+        }
+    }
+
     #endregion Functions
 
     #region Main
 
+    If (!$Domain -and (Get-WmiObject Win32_ComputerSystem).PartOfDomain)
+    {
+        $Domain = $env:USERDNSDOMAIN
+    }
+    If (!$Domain -and !(Get-WmiObject Win32_ComputerSystem).PartOfDomain)
+    {
+        $Domain = Read-Host 'Domain (FQDN)'
+    }
     IF ($Domain -notlike '*.*') {
         Throw 'You must specify a domain FQDN.'
     }
@@ -140,75 +217,38 @@ function Activate-KMSClient
     # Find out if there is a KMS host on the domain
 
     $findkms = nslookup -type=srv _vlmcs._tcp.$domain 2>&1
-    [regex]$regex = '.+?\.' + ($Domain -replace '\.','\.')
-    $kmshost = $regex.Matches($findkms) | ForEach-Object {$_.value} | select -Last 1
-
-    IF ($kmshost -match "can't find") {
-        throw 'No KMS host has been found. Please check the domain name.'
+    If ([string]$findkms -match '(?<=svr\s+hostname\s+=\s)(.+)')
+    {
+        $kmshost = $Matches[1].Split(' ')[0]
+        Write-Verbose "KMS Host found: $kmshost"
     }
     else {
-        $kmshost = $kmshost -replace ' '
-        Write-Verbose "KMS Host found: $kmshost"
+        throw 'No KMS host has been found. Please check the domain name.'
     }
 
     # Determine KMS client key to use
-
-    $OSversion = (Get-WmiObject -class Win32_OperatingSystem).Caption
-    Write-Verbose "Operating system is $OSversion"
-
-    switch -Regex ($OSversion) {
-        'Windows 8.1 Professional N'                     {$key = 'HMCNV-VVBFX-7HMBH-CTY9B-B4FXY';break}
-        'Windows 8.1 Professional'                       {$key = 'GCRJD-8NW9H-F2CDX-CCM8D-9D6T9';break}
-        'Windows 8.1 Enterprise N'                       {$key = 'TT4HM-HN7YT-62K67-RGRQJ-JFFXW';break}
-        'Windows 8.1 Enterprise'                         {$key = 'MHF9N-XY6XB-WVXMC-BTDCT-MKKG7';break}
-        'Windows Server 2012 R2 Standard'                {$key = 'D2N9P-3P6X9-2R39C-7RTCD-MDVJX';break}
-        'Windows Server 2012 R2 Datacenter'              {$key = 'W3GGN-FT8W3-Y4M27-J84CP-Q3VJ9';break}
-        'Windows Server 2012 R2 Essentials'              {$key = 'KNC87-3J2TX-XB4WP-VCPJV-M4FWM';break}
-        'Windows 8 Professional N'                       {$key = 'XCVCF-2NXM9-723PB-MHCB7-2RYQQ';break}
-        'Windows 8 Professional'                         {$key = 'NG4HW-VH26C-733KW-K6F98-J8CK4';break}
-        'Windows 8 Enterprise N'                         {$key = 'JMNMF-RHW7P-DMY6X-RF3DR-X2BQT';break}
-        'Windows 8 Enterprise'                           {$key = '32JNW-9KQ84-P47T8-D8GGY-CWCK7';break}
-        'Windows Server 2012 Standard'                   {$key = 'XC9B7-NBPP2-83J2H-RHMBY-92BT4';break}
-        'Windows Server 2012 Single Language'            {$key = '2WN2H-YGCQR-KFX6K-CD6TF-84YXQ';break}
-        'Windows Server 2012 Country Specific'           {$key = '4K36P-JN4VD-GDC6V-KDT89-DYFKP';break}
-        'Windows Server 2012 Server Standard'            {$key = 'XC9B7-NBPP2-83J2H-RHMBY-92BT4';break}
-        'Windows Server 2012 MultiPoint Standard'        {$key = 'HM7DN-YVMH3-46JC3-XYTG7-CYQJJ';break}
-        'Windows Server 2012 MultiPoint Premium'         {$key = 'XNH6W-2V9GX-RGJ4K-Y8X6F-QGJ2G';break}
-        'Windows Server 2012 Datacenter'                 {$key = '48HP8-DN98B-MYWDG-T2DCC-8W83P';break}
-        'Windows Server 2012 N'                          {$key = '8N2M2-HWPGY-7PGT9-HGDD8-GVGGY';break}
-        'Windows Server 2012'                            {$key = 'BN3D2-R7TKB-3YPBD-8DRP2-27GG4';break}
-        'Windows 7 Professional N'                       {$key = 'MRPKT-YTG23-K7D7T-X2JMM-QY7MG';break}
-        'Windows 7 Professional E'                       {$key = 'W82YF-2Q76Y-63HXB-FGJG9-GF7QX';break}
-        'Windows 7 Professional'                         {$key = 'FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4';break}
-        'Windows 7 Enterprise N'                         {$key = 'YDRBP-3D83W-TY26F-D46B2-XCKRJ';break}
-        'Windows 7 Enterprise E'                         {$key = 'C29WB-22CC8-VJ326-GHFJW-H9DH4';break}
-        'Windows 7 Enterprise'                           {$key = '33PXH-7Y6KF-2VJC9-XBBR8-HVTHH';break}
-        'Windows Server 2008 R2 Web'                     {$key = '6TPJF-RBVHG-WBW2R-86QPH-6RTM4';break}
-        'Windows Server 2008 R2 HPC edition'             {$key = 'TT8MH-CG224-D3D7Q-498W2-9QCTX';break}
-        'Windows Server 2008 R2 Standard'                {$key = 'YC6KT-GKW9T-YTKYR-T4X34-R7VHC';break}
-        'Windows Server 2008 R2 Enterprise'              {$key = '489J6-VHDMP-X63PK-3K798-CPX3Y';break}
-        'Windows Server 2008 R2 Datacenter'              {$key = '74YFP-3QFB3-KQT8W-PMXWJ-7M648';break}
-        'Windows Server 2008 R2 for Itanium'             {$key = 'GT63C-RJFQ3-4GMB6-BRFB9-CB83V';break}
-        'Windows Vista Business N'                       {$key = 'HMBQG-8H2RH-C77VX-27R82-VMQBT';break}
-        'Windows Vista Business'                         {$key = 'YFKBB-PQJJV-G996G-VWGXY-2V3X8';break}
-        'Windows Vista Enterprise N'                     {$key = 'VTC42-BM838-43QHV-84HX6-XJXKV';break}
-        'Windows Vista Enterprise'                       {$key = 'VKK3X-68KWM-X2YGT-QR4M6-4BWMV';break}
-        'Windows Web Server 2008'                        {$key = 'WYR28-R7TFJ-3X2YQ-YCY4H-M249D';break}
-        'Windows Server 2008 Standard without Hyper-V'   {$key = 'W7VD6-7JFBR-RX26B-YKQ3Y-6FFFJ';break}
-        'Windows Server 2008 Standard'                   {$key = 'TM24T-X9RMF-VWXK6-X8JC9-BFGM2';break}
-        'Windows Server 2008 Enterprise without Hyper-V' {$key = '39BXF-X8Q23-P2WWT-38T2F-G3FPG';break}
-        'Windows Server 2008 Enterprise'                 {$key = 'YQGMW-MPWTJ-34KDK-48M3W-X4Q6V';break}
-        'Windows Server 2008 HPC'                        {$key = 'RCTX3-KWVHP-BR6TB-RB6DM-6X7HP';break}
-        'Windows Server 2008 Datacenter without Hyper-V' {$key = '22XQ2-VRXRG-P8D42-K34TD-G3QQC';break}
-        'Windows Server 2008 Datacenter'                 {$key = '7M67G-PC374-GR742-YH8V4-TCBY3';break}
-        'Windows Server 2008 for Itanium-Based Systems'  {$key = '4DWFP-JF3DJ-B7DTH-78FJB-PDRHK';break}
-    }
-
-    IF (!$key) {
-        throw "No KMS client key was found for operating system '$OSVersion'."
-    }
-    else {
-        Write-Verbose "KMS client key is $key"
+    If ($AutoDetectKey)
+    {
+        $OSversion = (Get-WmiObject -class Win32_OperatingSystem).Caption.Trim()
+        Write-Verbose "Operating system is $OSversion"
+        $KMSKeys = Get-KMSClientKeys
+        foreach ($KMSKey in $KMSKeys)
+        {
+            $KMSobject = $KMSKey.OperatingSystem.Split(' ') | where {$_ -ne 'server'}
+            $Caption = $OSversion.TrimStart('Microsoft ').Split(' ') | where {$_ -ne 'server'}
+            $Compared = Compare $KMSobject $caption
+            If ($Compared.SideIndicator -notcontains '<=')
+            {
+                $ClientKey = $KMSKey.KMSClientKey
+                Write-Verbose "Found KMS OS: $($KMSKey.OperatingSystem)"
+            }
+        }
+        IF (!$ClientKey) {
+            throw "No KMS client key was found for operating system '$OSVersion'."
+        }
+        else {
+            Write-Verbose "KMS client key is $ClientKey"
+        }
     }
 
     # Deactivate Windows if necessary
@@ -229,7 +269,7 @@ function Activate-KMSClient
     if ($pscmdlet.ShouldProcess($hostname, "Activating Windows"))
     {
         Write-Verbose 'Activating Windows.'
-        $null = $KMSservice.InstallProductKey($key)
+        $null = $KMSservice.InstallProductKey($ClientKey)
         IF (($KMSservice | gm -Name SetKeyManagementServiceLookupDomain) -ne $null) {
             $null = $KMSservice.SetKeyManagementServiceLookupDomain($Domain)
         }
