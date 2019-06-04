b7a7a2f7cb6850c2587da7ff652d0f78f15651f5
Michaja van der Zouwen <ITMicaH@users.noreply.github.com>
2019-06-04 09:25:31 +0200

    Create Elevate.ps1

---
+++ PowerShell/Elevate.ps1
@@ -0,0 +1,43 @@
+<#
+.Synopsis
+   Run powershell command as admin
+.DESCRIPTION
+   Run a powershell command with elevated rights without relaunching
+   the whole script as admin.
+.EXAMPLE
+   Invoke-ElevatedCommand -Command Stop-Process -Parameters @{name = 'chrome';force = $true}
+.EXAMPLE
+   $Params = @{
+        name = 'chrome'
+        force = $true
+   }
+   Elevate Stop-Process $Params
+#>
+function Invoke-ElevatedCommand
+{
+    [CmdletBinding()]
+    [Alias('Elevate')]
+    Param
+    (
+        # Command to run
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]
+        $Command,
+
+        # Parameters in hashtable format (like with splatting)
+        [Parameter(Position=1)]
+        [hashtable]
+        $Parameters
+
+    )
+    $Params = $Parameters.GetEnumerator().foreach{"-$($_.key) '$($_.value)'"} -join ' '
+    $ProcessParams = @{
+        FilePath = 'powershell.exe'
+        Verb = 'RunAs'
+        ArgumentList = "-command & {$Command $Params}"
+        Wait = $true
+        WindowStyle = 'Hidden'
+    }
+    Start-Process @ProcessParams
+}
