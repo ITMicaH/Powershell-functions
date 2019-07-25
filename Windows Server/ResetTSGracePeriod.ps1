57cfad47903529929f6863930b14b9716c841d5a
Michaja van der Zouwen <ITMicaH@users.noreply.github.com>
2019-07-25 09:15:54 +0200

    Update ResetTSGracePeriod.ps1

--- Windows Server/ResetTSGracePeriod.ps1
+++ Windows Server/ResetTSGracePeriod.ps1
@@ -3,7 +3,8 @@
    Reset Terminal Services Grace period
 .DESCRIPTION
    Resets the Terminal Services grace period if it's below 20 days left. This script should be run using
-   a scheduled task (Task Manager) under the system account.
+   a scheduled task (Task Manager) under the system account. If you place this script in C:\Temp you can import
+   the xml file in Task Scheduler.
 #>
 
 $Threshold = 20
