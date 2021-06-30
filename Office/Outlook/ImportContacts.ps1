9f41941afb114cef74496b65db2a85d2f4ea958c
Michaja van der Zouwen <ITMicaH@users.noreply.github.com>
2021-06-30 16:10:08 +0200

    Added overwrite option

--- Office/Outlook/ImportContacts.ps1
+++ Office/Outlook/ImportContacts.ps1
@@ -5,11 +5,15 @@
    This Powershell function will allow you to create outlook contacts using a csv file as input.
    The csv has to contain headers that match the properties for the contact objects.
 .EXAMPLE
-   Import-OutlookContacts -FilePath "C:\Users\Pete\Documents\Contacts.csv"
+   Import-OutlookContacts -FilePath "$([Environment]::GetFolderPath('MyDocuments'))\Contacts.csv"
 
    This will create Outlook contacts based on the Contacts.csv file.
 .EXAMPLE
-   Import-OutlookContacts -FilePath "C:\Users\Pete\Documents\Contacts.csv" -SubFolder NewContacts\Test
+   Import-OutlookContacts -FilePath "$([Environment]::GetFolderPath('MyDocuments'))\Contacts.csv" -Overwrite
+
+   This will create Outlook contacts based on the Contacts.csv file and overwrite existing contacts
+.EXAMPLE
+   Import-OutlookContacts -FilePath "$([Environment]::GetFolderPath('MyDocuments'))\Contacts.csv" -SubFolder NewContacts\Test
 
    This will create Outlook contacts based on the Contacts.csv file in the subfolder NewContacts\Test.
 .EXAMPLE
@@ -17,7 +21,7 @@
 
    This will generate a list of possible headers to use for your csv file.
 .EXAMPLE
-   Import-OutlookContacts -GenerateEmptyCSV -FilePath "C:\Users\Ryan\Documents\Contacts.csv"
+   Import-OutlookContacts -GenerateEmptyCSV -FilePath "$([Environment]::GetFolderPath('MyDocuments'))\Contacts.csv"
 
    This will create an empty CSV file with all possible headers.
 .LINK
@@ -49,17 +53,31 @@ function Import-OutlookContacts
         # Only list the possible headers
         [Parameter(ParameterSetName='ListOnly')]
         [switch]
-        $ListPossibleHeaders
+        $ListPossibleHeaders,
+
+        # Overwrite existing contacts based on the email address
+        [switch]
+        $Overwrite
     )
 
     #region Functions
 
     function Add-Contact 
     {
-        param ($folder,$user,$properties)
+        param ($folder,$user,$properties,[switch]$Overwrite)
 
+        $OldContact = @($folder.Items).Where{$_.Email1Address -eq $user.Email1Address}
+        if ($OldContact -and $PSBoundParameters.Overwrite)
+        {
+            $OldContact.Delete()
+        }
+        elseif ($OldContact)
+        {
+            Write-Warning "Skipping contact with email address $($user.Email1Address)"
+            return
+        }
+        
         $newcontact = $folder.Items.Add()
-
         Foreach ($property in $properties){
             IF ($user.$property) {
                 $newcontact.$property = $user.$property
@@ -113,7 +131,8 @@ function Import-OutlookContacts
 
     $properties = Get-ContactProperties
 
-    IF ($ListPossibleHeaders){
+    IF ($PSBoundParameters.ListPossibleHeaders)
+    {
         Write-Host ""
         Write-Host "---------------------------------------"
         Write-Host "Available headers for the CSV file are:"
@@ -123,20 +142,21 @@ function Import-OutlookContacts
         return
     }
 
-    IF ($GenerateEmptyCSV) {
+    IF ($PSBoundParameters.GenerateEmptyCSV) 
+    {
         $properties | select $properties | Export-Csv $FilePath -UseCulture -NoTypeInformation
         return
     }
 
     # Import CSV
-    $csv = Import-Csv $FilePath -UseCulture
+    $csv = @(Import-Csv $FilePath -UseCulture)
 
     # Add contacts
     for ($i = 0; $i -lt $csv.count; $i++)
     { 
         [int]$Completion = $i/$csv.count*100
         Write-Progress -Activity "Adding contacts to outlook" -Status "$Completion% complete" -PercentComplete $Completion
-        Add-Contact $folder $csv[$i] $properties
+        Add-Contact $folder $csv[$i] $properties -Overwrite:$Overwrite
     }
     #endregion Main Script
 }
