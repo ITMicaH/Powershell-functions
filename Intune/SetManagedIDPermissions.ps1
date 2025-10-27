54b1d5b44a9097b905c7bc2c5349257622e586f9
Michaja van der Zouwen <ITMicaH@users.noreply.github.com>
2025-10-27 15:48:07 +0100

    Update Managed ID permissions and connection logic

--- Intune/SetManagedIDPermissions.ps1
+++ Intune/SetManagedIDPermissions.ps1
@@ -1,13 +1,15 @@
 #region Variables
 
 $Permissions = 'Group.Read.All','GroupMember.Read.All','Mail.Send','ServiceMessage.Read.All'
-$ManagedIdentity = '<Name of the Managed ID>'
+$ManagedIdentity = 'pwexpirationdate'
+$Type = 'Graph' #or Exchange
 
 #endregion Variables
 
 #region constants
 
 $GraphAppId = "00000003-0000-0000-c000-000000000000"
+$ExchangeAppId = "00000002-0000-0ff1-ce00-000000000000"
 
 #endregion constants
 
@@ -15,21 +17,32 @@ $GraphAppId = "00000003-0000-0000-c000-000000000000"
 
 if (!(Get-MgContext))
 {
-    Connect-MgGraph -Identity -Scopes Directory.Read.All,AppRoleAssignment.ReadWrite.All,Application.Read.All
+    if ($psISE)
+    {
+        Connect-TKGraph
+    }
+    else
+    {
+        Connect-MgGraph -Identity 
+    }
 }
 
 #endregion Connection
 
 #region Main
-
+switch ($Type)
+{
+    Graph    {$AppID = $GraphAppId}
+    Exchange {$AppID = $ExchangeAppId}
+}
 $ManagedID = Get-MgServicePrincipal -Filter "displayName eq '$ManagedIdentity'"
-$GraphServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '$GraphAppId'"
-$AppRoles = $GraphServicePrincipal.AppRoles.where{$_.Value -in $Permissions -and $_.AllowedMemberTypes -contains "Application"}
+$ServicePrincipal = Get-MgServicePrincipal -Filter "appId eq '$AppId'"
+$AppRoles = $ServicePrincipal.AppRoles.where{$_.Value -in $Permissions -and $_.AllowedMemberTypes -contains "Application"}
 
 $Params = @{
     ServicePrincipalId = $ManagedID.Id
     PrincipalId = $ManagedID.Id
-    ResourceId  = $GraphServicePrincipal.Id
+    ResourceId  = $ServicePrincipal.Id
     AppRoleId   = ''
 }
 
